0f31fb0c4869b5a75d4fce643bf41942a3bcd6f1
B_Wilhelm <bwilhelm@iastate.edu>
2017-11-21 17:47:39 -0600

    Dunno

--- core/src/com/wilhelm/notaclicker/GameScreen.java
+++ core/src/com/wilhelm/notaclicker/GameScreen.java
@@ -57,7 +57,6 @@ class GameScreen implements Screen {
     public void render (float delta) {
         onBackPressed();
 
-
 //        Gdx.gl.glClearColor(44f/255f, 182f/255f, 216f/255f, 1);
         Gdx.gl.glClearColor(200f/255f, 200f/255f, 200f/255f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
